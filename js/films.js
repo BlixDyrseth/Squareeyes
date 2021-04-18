9bd551a0625c9b55471a984c48166a7f3d264248
BlixDyrseth <71275233+BlixDyrseth@users.noreply.github.com>
2021-04-18 10:43:14 +0200

    update

--- js/films.js
+++ js/films.js
@@ -1,8 +1,8 @@
-const url = "http://ingridblixdyrseth.no/wp-json/wc/store/products?category";
+const url = "http://ingridblixdyrseth.no/wp-json/wc/store/products?_embed/";
 
 const filmBlock = document.querySelector(".search-block")
 
-const filmContainer = document.querySelector(".films")
+const filmContainer = document.querySelector(".thriller")
 
 async function getFilms() {
     try {
@@ -16,14 +16,13 @@ async function getFilms() {
         filmContainer.innerHTML = "";
 
         films.forEach(function(film) {
-            filmContainer.innerHTML += `<h2 class="h2-search">${film.categories.name}</h2>
-            <a href="newfilmpage.html?id=${film.id}" class="a-search">
+            filmContainer.innerHTML += `<a href="newfilmpage.html?id=${film.id}" class="a-search">
             <div class="image" style="background-image: url('${film.images.thumbnail}')"</div>
             <div class="search-block">
             <h4>${film.name}</h4>
-            <p>Price: ${film.prices.currency_symbol} ${film.prices.price}</p>
-            </a>
-            </div` 
+            <h4>Price: ${film.prices.currency_symbol} ${film.prices.price}</h4>
+            </div
+            </a>` 
         }); 
     }
     catch(error) {
